d356878ac78942a960636e4d1795e3e621e26739
William Colmenares <colmenares.william@gmail.com>
2020-07-17 12:07:47 -0400

    added threads

--- tribal/views.py
+++ tribal/views.py
@@ -2,9 +2,39 @@ from rest_framework.response import Response
 from .serializers import ResultSerializer
 from rest_framework.views import APIView
 from rest_framework import status
+import concurrent.futures
 from zeep import Client
 import requests
 import json
+
+def people_search(query):
+    results = []
+    c = Client(wsdl='http://www.crcind.com/csp/samples/SOAP.Demo.CLS?wsdl')
+    people = c.service.GetListByName(query)
+    if people is not None: 
+        for i in people:
+            results.append({'name': i.Name, 'type_': 'Person', 'source': 'crcind.com'})
+    return results
+
+def itunes_search(query):
+    results = []
+    r = requests.get(f'https://itunes.apple.com/search?term={query}&media=music&media=movie')
+    if not r.json():
+        return results
+    else: 
+        for i in r.json()['results']:
+            results.append({'name': i['trackName'], 'type_': i['kind'], 'source': 'apple.com'})
+        return results
+
+def tvmaze_search(query):
+    results = []
+    r = requests.get(f'http://api.tvmaze.com/search/shows?q={query}')
+    if not r.json():
+        return results
+    else:
+        for tvshow in r.json():
+            results.append({'name': tvshow['show']['name'], 'type_': 'show', 'source': 'tvmaze.com'})
+        return results
 
 class TribalSearch(APIView):
     """
@@ -18,25 +48,12 @@ class TribalSearch(APIView):
         if not query:
             return Response({'Response': "You need to submit a query"}, status=status.HTTP_400_BAD_REQUEST)
         else:
+            functions = [people_search, itunes_search, tvmaze_search]
             results = []
-            c = Client(wsdl='http://www.crcind.com/csp/samples/SOAP.Demo.CLS?wsdl')
-
-            people = c.service.GetListByName(query)
-            if people is not None:
-                for i in people:
-                    results.append({'name': i.Name, 'type_': 'Person', 'source': 'crcind.com'})
-            
-            query = query.replace(' ', '+')
-            # Replacing to allow users to input spaces without issues.
-            
-            r = requests.get(f'https://itunes.apple.com/search?term={query}&media=music&media=movie')
-            for i in r.json()['results']:
-                results.append({'name': i['trackName'], 'type_': i['kind'], 'source': 'apple.com'})
-            
-            r = requests.get(f'http://api.tvmaze.com/search/shows?q={query}')
-            for tvshow in r.json():
-                results.append({'name': tvshow['show']['name'], 'type_': 'show', 'source': 'tvmaze.com'})
-
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                funcresults = [executor.submit(func, query) for func in functions]
+                for f in concurrent.futures.as_completed(funcresults):
+                    results = results + f.result()
             return Response(ResultSerializer(results, many=True).data)
 
             
